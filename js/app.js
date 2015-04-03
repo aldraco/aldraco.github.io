e339fffc2be565682f98f07bc9b8c26269496adf
A. Drake <a.l.drake713@gmail.com>
2015-04-03 14:32:13 -0400

    Added masonry example and Angular to the project as a whole.

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//app.js
+
+var app = angular.module('ToolsApp', ['wu.masonry'])
+.controller('BricksCtrl', ['$scope', function($scope) {
+	function genBrick() {
+        var height = ~~(Math.random() * 500) + 100;
+        var id = ~~(Math.random() * 10000);
+        return {
+            src: 'http://lorempixel.com/g/230/' + height + '/?' + id
+        };
+    };
+
+    $scope.bricks = [
+        genBrick(),
+        genBrick(),
+        genBrick(),
+        genBrick(),
+        genBrick(),
+        genBrick(),
+        genBrick(),
+        genBrick()
+    ];
+
+    /*$scope.add = function add() {
+        $scope.bricks.push(genBrick());
+    };
+
+    $scope.remove = function remove() {
+        $scope.bricks.splice(
+            ~~(Math.random() * $scope.bricks.length),
+            1
+        )
+    };*/
+
+
+}]);
